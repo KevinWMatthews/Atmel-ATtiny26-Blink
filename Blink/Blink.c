d28522b72d4a67ebd930331cbee608f928f6433c
Kevin Matthews <KevinWMatthews@gmail.com>
2015-04-13 09:29:04 -0700

    Initial Commit

---
+++ Blink/Blink.c
@@ -0,0 +1,19 @@
+/*
+ * Blink.c
+ *
+ * Created: 4/10/2015 15:33:31 PM
+ *  Author: Kevin Matthews
+ *
+ * CPU: ATtiny26-16PU
+ */
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+  while(1)
+  {
+    //TODO:: Please write your application code
+  }
+}
