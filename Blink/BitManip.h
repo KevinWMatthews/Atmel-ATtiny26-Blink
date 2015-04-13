a40d46e8179962777344c68f88052b7a232c97df
Kevin Matthews <KevinWMatthews@gmail.com>
2015-04-13 09:51:16 -0700

    Toggle PA0 (pin 20)

--- Blink/BitManip.h
+++ Blink/BitManip.h
@@ -2,5 +2,6 @@
 #define __BitManip_H
 
 #define sbm(VAR, BITMASK) ((VAR) |= (BITMASK))    /* Set BitMask */
+#define cbm(VAR, BITMASK) ((VAR) &= ~(BITMASK))   /* Clear BitMask */
 
 #endif
--- Blink/Blink.c
+++ Blink/Blink.c
@@ -7,20 +7,28 @@
  * CPU: ATtiny26-16PU
  */
 
+#ifndef F_CPU               /* Used by <util/delay.h> */
+#define F_CPU 8000000UL     /* 8MHz internal crystal */
+#endif
 
 #include <avr/io.h>
+#include <util/delay.h>
 #include "BitManip.h"
 
 #define TEST_PIN (1<<PA0)
 
 int main(void)
 {
+  int delay_time = 2000;
+
   /* Configure pin as output */
   DDRA = TEST_PIN;
 
-  sbm(PORTA, TEST_PIN);
-
   while(1)
   {
+    sbm(PORTA, TEST_PIN);
+    _delay_ms(delay_time);
+    cbm(PORTA, TEST_PIN);
+    _delay_ms(delay_time);
   }
 }
